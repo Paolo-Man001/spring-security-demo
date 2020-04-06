43a131cab314dd85f1dd89428a1f26504aba4a44
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-04-06 22:57:50 +1200

    s3.v1 : Overrride Default User and Password :   Override method UserDetailsService and with Userbuilder,   setup username, password, and roles. Return the InMemoryUserDetailsManager(UserDetail)   Pass UserDetail-typeOf-var that holds new user-detail info, as args.

--- src/main/java/com/example/springsecuritydemo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/springsecuritydemo/security/ApplicationSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.example.springsecuritydemo.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -12,11 +17,24 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
    protected void configure(HttpSecurity http) throws Exception {
       http
               .authorizeRequests()
-              .antMatchers("/","index","/css/*","/js/*")
+              .antMatchers("/", "index", "/css/*", "/js/*")
               .permitAll()
               .anyRequest()
               .authenticated()
               .and()
-              .httpBasic();
+              .httpBasic();   // refers to the type of authentication we want -> basicAuth
+   }
+
+   // override the default user: 'user' and password: 'Random_UUID'
+   @Override
+   @Bean       // Bean will instantiate this for us
+   protected UserDetailsService userDetailsService() {
+      UserDetails annaSmithUser = User.builder()
+              .username("annasmith")
+              .password("password")
+              .roles("STUDENT")  // ROLE_STUDENT is how Spring Interpret this line.
+              .build();
+
+      return new InMemoryUserDetailsManager(annaSmithUser);
    }
 }
---
+++ src/main/java/com/example/springsecuritydemo/security/PasswordConfig.java
@@ -0,0 +1,4 @@
+package com.example.springsecuritydemo.security;
+
+public class PasswordConfig {
+}
